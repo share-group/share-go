01ff3b90353384d65a572711c6df7f15908eca34
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2024-01-02 16:17:59 +0800

    增加常用工具库

--- util/random.go
+++ util/random.go
@@ -15,12 +15,12 @@ func newRandomUtil() *randomUtil {
 // 生成随机字符串
 //
 // length-字符串长度
-func String(length int) string {
+func (r *randomUtil) String(length int) string {
 	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	bytes := []byte(str)
 	result := []byte{}
 	for i := 0; i < length; i++ {
-		result = append(result, bytes[Int(0, len(bytes)-1)])
+		result = append(result, bytes[r.Int(0, len(bytes)-1)])
 	}
 	return string(result)
 }
@@ -29,6 +29,6 @@ func String(length int) string {
 //
 // min-最小值
 // max-最大值
-func Int(min int, max int) int {
+func (r *randomUtil) Int(min int, max int) int {
 	return rand.Intn(max-min+1) + min
 }
