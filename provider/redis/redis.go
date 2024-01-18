c1b3743e19abcb4c48ad72f394da16e17a465cd8
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2024-01-18 13:19:30 +0800

    fix bug

--- provider/redis/redis.go
+++ provider/redis/redis.go
@@ -5,7 +5,7 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/share-group/share-go/provider/config"
 	loggerFactory "github.com/share-group/share-go/provider/logger"
-	"github.com/share-group/share-go/util"
+	"github.com/share-group/share-go/util/systemutil"
 	"os"
 )
 
@@ -22,7 +22,7 @@ var redisClient *redis.Client
 func NewRedis(host, password string, db int) *redisObj {
 	redisClient = redis.NewClient(&redis.Options{Addr: host, Password: password, DB: db})
 	ping, err := redisClient.Ping().Result()
-	logger.Info(fmt.Sprintf("redis connect %v %s ...", host, util.SystemUtil.If(ping == "PONG", "success", "fail")))
+	logger.Info(fmt.Sprintf("redis connect %v %s ...", host, systemutil.If(ping == "PONG", "success", "fail")))
 	if err != nil {
 		logger.Fatal(fmt.Sprintf("%v", err))
 		os.Exit(1)
